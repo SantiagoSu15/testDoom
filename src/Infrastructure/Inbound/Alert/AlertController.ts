5fd6236c338e5a18f45ede0c7cdf76f5d493819f
SantiagoSu15 <sasupu08@gmail.com>
2026-07-01 23:13:10 -0500

    Caso de uso boton de alerta

--- src/Infrastructure/Inbound/Alert/AlertController.ts
+++ src/Infrastructure/Inbound/Alert/AlertController.ts
@@ -1,29 +1,30 @@
-
-import { Body, Controller, Get, Inject, Param, Patch, Post, Put } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import type { AcceptAlertUseCase } from "src/Application/Ports/In/Alerts/AcceptAlertUseCase";
-import { ALERT_PORTS } from "src/Application/Ports/Out/AlertTokens";
-import { AlertButtonDTO } from "./AlertDTOS";
+import { Body, Controller, Inject, Param, Patch, Put } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import type { AcceptAlertUseCase } from 'src/Application/Ports/In/Alerts/AcceptAlertUseCase';
+import { ALERT_PORTS } from 'src/Application/Ports/Out/AlertTokens';
+import { AlertButtonDTO } from './AlertDTOS';
+import type { UsePanicButtonUseCase } from '../../../Application/Ports/In/Alerts/UsePanicButtonUseCase';
 
 @ApiTags('Alert')
 @Controller('alerts')
 export class AlertController {
+  constructor(
+    @Inject(ALERT_PORTS.manageAlert)
+    private readonly manageAlertUseCase: AcceptAlertUseCase,
+    @Inject(ALERT_PORTS.UsePanicButtonUseCase)
+    private readonly usePanicButtonUseCase: UsePanicButtonUseCase,
+  ) {}
 
+  @Patch('manage/:id')
+  manageAlert(@Param('id') id: string, @Body('confirm') confirm: boolean) {
+    return this.manageAlertUseCase.AcceptAlert(id, confirm);
+  }
 
-    constructor(
-        @Inject(ALERT_PORTS.manageAlert)
-        private readonly manageAlertUseCase: AcceptAlertUseCase
-    ){}
-
-
-    @Patch('manage/:id')
-    manageAlert(@Param('id') id : string,@Body('confirm') confirm : boolean){
-        return this.manageAlertUseCase.AcceptAlert(id,confirm);
-    }
-
-    @Put('panic-button/:id')
-    activePanicButton(@Param('id') userId : number,@Body('alertButtonDTO') dto : AlertButtonDTO){
-
-    }
-
+  @Put('panic-button/:id')
+  activePanicButton(
+    @Param('id') userId: number,
+    @Body('alertButtonDTO') dto: AlertButtonDTO,
+  ) {
+    return this.usePanicButtonUseCase.UsePanicButton(userId, dto);
+  }
 }
